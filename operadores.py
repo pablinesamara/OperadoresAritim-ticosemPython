18807c7a56a2ee0b2a347de204f26e03d8943046
Pabline Ribeiro <119081902+pablinesamara@users.noreply.github.com>
2024-05-14 20:38:13 -0300

    Create operadores.py

---
+++ operadores.py
@@ -0,0 +1,14 @@
+#Adição
+print(1+1)
+#subtracao
+print(10-1)
+#multiplicação
+print(4*3)
+#Divisão que retorna float
+print(12/3)
+#Divisão que retorna somente o valor inteiro
+print(12//2)
+#Modulo
+print(10%3)
+#exponenciacao ex 2³
+print(2**3)
